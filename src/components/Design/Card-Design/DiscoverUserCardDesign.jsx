329e21509842e681dd84f0d1401c4f09740bac87
ehasun15019 <ehasunulislam0202@gmail.com>
2026-01-28 01:46:53 +0600

    suppose have 2 man A'B. if A send a friend-request then show the result of pending and A can cancel the request

--- src/components/Design/Card-Design/DiscoverUserCardDesign.jsx
+++ src/components/Design/Card-Design/DiscoverUserCardDesign.jsx
@@ -1,11 +1,50 @@
 "use client";
 
 import useAuthInfo from "@/Hooks/useAuthInfo";
+import useAxios from "@/Hooks/useAxios";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const DiscoverUserCardDesign = ({ img, name, email }) => {
   const { user } = useAuthInfo();
+  const axiosSecure = useAxios();
+
+  const [status, setStatus] = useState("none");
+
+  // handleConnect functionality
+  const handleConnect = async () => {
+    // send request
+    if(status === "none") {
+      const res = await axiosSecure.post("/friend-request/send", {
+        senderEmail: user.email,
+        receiverEmail: email,
+      });
+  
+      setStatus(res.data.status);
+    }
+
+    if(status === "pending") {
+      const res = await axiosSecure.delete("/friend-request/cancel", {
+        data: {
+          senderEmail: user.email,
+          receiverEmail: email,
+        }
+      });
+
+      setStatus(res.data.status);
+    }
+  };
+
+  // handle staus when page reload then the status should not be changed
+  useEffect(() => {
+    if(!user?.email) return;
+
+    axiosSecure.get(`/friend-request/status?senderEmail=${user.email}&receiverEmail=${email}`)
+    .then((res) => {
+      setStatus(res.data.status)
+    })
+
+  }, [user?.email, axiosSecure, email])
 
   return (
     <div
@@ -25,7 +64,15 @@ const DiscoverUserCardDesign = ({ img, name, email }) => {
         <h4 className="text-[0.593rem] md:text-[0.75rem] font-bold">{name}</h4>
         {/* <p>{email}</p> */}
         <div className="card-actions w-full flex flex-col justify-center items-center text-center">
-          <button className="px-6 py-1 bg-transparent border border-gray-400 text-black rounded-full cursor-pointer">Connect</button>
+          <button
+
+            className={`px-6 py-1 bg-transparent border border-gray-400 text-black rounded-full cursor-pointer ${status === "pending" ? "text-gray-400" : ""}`}
+            onClick={handleConnect}
+          >
+            {status === "none" && "connect"}
+            {status === "pending" && "Pending"}
+            {status === "connected" && "Connected"}
+          </button>
         </div>
       </div>
     </div>
