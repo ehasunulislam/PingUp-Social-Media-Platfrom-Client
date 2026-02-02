56566994e663c80f77dcc9e88e03e1fb4abe8d13
ehasun15019 <ehasunulislam0202@gmail.com>
2026-02-03 03:13:18 +0600

    inital update

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -3,7 +3,7 @@ import RegisterClient from "./RegisterClient";
 
 export default function Page() {
     return(
-        <Suspense fallback={null}>
+        <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
             <RegisterClient />
         </Suspense>
     )
