791a924d6822b082f3b74a5b43a44698d51fee08
ehasun15019 <ehasunulislam0202@gmail.com>
2026-02-03 03:06:08 +0600

    update the register functionality

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,5 +1,5 @@
-import RegisterClient from "@/app/RegisterClient";
 import { Suspense } from "react";
+import RegisterClient from "./RegisterClient";
 
 export default function Page() {
     return(
